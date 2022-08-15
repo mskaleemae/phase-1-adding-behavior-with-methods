8018d676ad756cdfc8eaaf91b61d99897d5192a5
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-15 15:02:56 -0400

    commit tree 83fd70517db15c6058755f081314a3f6b06a7d18

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class Cat {
+constructor(name,sex) {
+    this.name = name;
+    this.sex = sex;
+}
+saySpeak() {
+    console.log(`${this.name} says meow!`)
+}
+}
+
+class Dog {
+    constructor(name,sex) {
+        this.name = name;
+        this.sex = sex;  
+    }
+    saySpeak() {
+        console.log(`${this.name} says woof!`)
+    }
+ }
+
+class Bird {
+    constructor(name,sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    saySpeak() {
+        
+    }
+}
